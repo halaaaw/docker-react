88e817fda4177a439cd2184d90b142dbea42511e
Hala Mohammad <hmohammad@rapidminer.com>
2022-02-23 23:38:59 +0100

    change 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there! Halaaaaaaaaaaaaaaa22cc</p>
+        <p>alhamdulillah rab el3alameeeeeeeeeeeeeeeeen</p>
         <a
           className="App-link"
           href="https://reactjs.org"
